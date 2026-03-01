972d0e44c77348a4be3d387da838034d0fa2250b
malikmillerdev <malikmillerbusiness96@gmail.com>
2026-03-01 15:19:35 -0500

    E: Removing the Screen print

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ def main():
                 return
         screen.fill("black")
         print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
