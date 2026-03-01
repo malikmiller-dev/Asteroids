d6cbb0216c8cbaa14d0be6f5030dea50123b2a62
malikmillerdev <malikmillerbusiness96@gmail.com>
2026-03-01 11:58:56 -0500

    B: Made significant updates to main.py, player.py, and circleshape.py Also added asteroids.py. Continuing to update asteroids.py.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
+
+from circleshape import CircleShape
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
+    """ Main function to run Asteroids game """
+    x = (SCREEN_WIDTH / 2)
+    y = (SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
     pygame.init()
     clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     while True:
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        updatable.update(dt)
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +27,8 @@ def main():
         print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
